3c931089a22444c0cc82edaa6a5afc66480c5d8a
Ahmad AR <38329562+Ahmadar91@users.noreply.github.com>
2019-12-09 22:09:10 +0100

    express

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 'use strict'
-console.log('hello express!')
+const express = require('express')
+const app = express()
+
+// routes
+app.use('/', (req, res) => {
+  res.send('Hello Express!')
+})
+// listen to provided port
+app.listen(3000, () => console.log('server is running on http://localhost:3000')
+)
