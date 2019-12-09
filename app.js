80aa6514431e737b8b1d5118175ad82ed4747fdc
Ahmad AR <38329562+Ahmadar91@users.noreply.github.com>
2019-12-09 22:20:47 +0100

    started

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const express = require('express')
 const app = express()
 
 // routes
-app.use('/', (req, res) => {
-  res.send('Hello Express!')
-})
+app.use('/', require('./routes/homeRouter'))
 // listen to provided port
 app.listen(3000, () => console.log('server is running on http://localhost:3000')
 )
