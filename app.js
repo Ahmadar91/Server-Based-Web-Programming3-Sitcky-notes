8f3a2767c4991376c98a79ecbb8d6437dd4b58fa
Ahamd <aa224fn@student.lnu.se>
2019-12-10 03:12:42 +0100

    404 and 500

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const hbs = require('express-hbs')
 const path = require('path')
-
+const logger = require('morgan')
 const app = express()
 
 // view engine setup
@@ -12,9 +12,22 @@ app.engine('hbs', hbs.express4({
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 // additional middleware
+app.use(logger('dev'))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 // routes
 app.use('/', require('./routes/homeRouter'))
+// catch 404
+app.use((req, res, next) => {
+  res.status(404)
+  res.sendFile(path.join(__dirname, 'public', '404.html'))
+})
+// error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.send(err.message || 'Internal Server Error')
+})
+
 // listen to provided port
 app.listen(3000, () => console.log('server is running on http://localhost:3000')
 )
