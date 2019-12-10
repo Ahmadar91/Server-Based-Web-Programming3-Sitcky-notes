c7664f93f686ff2285964746970123bb064a0f8e
Ahamd <aa224fn@student.lnu.se>
2019-12-10 02:43:39 +0100

    views

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 'use strict'
 const express = require('express')
+const hbs = require('express-hbs')
+const path = require('path')
+
 const app = express()
 
+// view engine setup
+app.engine('hbs', hbs.express4({
+  defaultLayout: path.join(__dirname, 'views', 'layouts', 'default')
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
 // routes
 app.use('/', require('./routes/homeRouter'))
 // listen to provided port
