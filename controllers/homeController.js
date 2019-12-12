60574e4ed48c317ab8246bb298037ef107dd9fcf
Ahmad AR <38329562+Ahmadar91@users.noreply.github.com>
2019-12-12 01:30:23 +0100

    fixed home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,18 @@
 const homeController = {}
-homeController.index = (req, res, next) => res.render('home/index')
+const CodeSnippet = require('../models/CodeSnippet')
+
+homeController.index = async (req, res, next) => {
+  try {
+    const viewData = {
+      codeSnippets: (await CodeSnippet.find({}))
+        .map(codeSnippet => ({
+          id: codeSnippet._id,
+          description: codeSnippet.description
+        }))
+    }
+    res.render('home/index', { viewData })
+  } catch (error) {
+    next(error)
+  }
+}
 module.exports = homeController
