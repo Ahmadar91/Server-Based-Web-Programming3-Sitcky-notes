7d944a0053f75b8c503767cb7d25809929a3c308
Ahamd <aa224fn@student.lnu.se>
2019-12-14 22:38:40 +0100

    final

--- routes/snippetRouter.js
+++ routes/snippetRouter.js
@@ -14,10 +14,10 @@ router.get('/', controller.index)
 //   .get(controller.create)
 //   .post(controller.createPost)
 router.get('/create', controller.create)
-router.post('/create', [check('title', 'Title is required').notEmpty().escape(), check('description', 'snippet is required').notEmpty().escape()], controller.createPost)
+router.post('/create', [check('title', 'Title is required').notEmpty(), check('description', 'snippet is required').notEmpty()], controller.createPost)
 // GET, POST /edit
 router.get('/edit/:id', controller.edit)
-router.post('/edit', [check('title', 'Title is required').notEmpty().escape(), check('description', 'snippet is required').notEmpty().escape()], controller.editPost)
+router.post('/edit', [check('title', 'Title is required').notEmpty(), check('description', 'snippet is required').notEmpty()], controller.editPost)
 
 // GET, POST  /delete
 router.get('/delete/:id', controller.delete)
